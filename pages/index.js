60b4049e9c9c5fccd398c589fc1d31e633432fee
Jimmy Thomas <jthomas@deezer.com>
2020-04-30 23:08:54 +0200

    cheh

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import { withApollo } from '../apollo/client'
-import gql from 'graphql-tag'
-import Link from 'next/link'
-import { useQuery } from '@apollo/react-hooks'
+import { withApollo } from "../apollo/client";
+import gql from "graphql-tag";
+import Link from "next/link";
+import { useQuery } from "@apollo/react-hooks";
 
 const ViewerQuery = gql`
   query ViewerQuery {
@@ -11,26 +11,26 @@ const ViewerQuery = gql`
       status
     }
   }
-`
+`;
 
 const Index = () => {
   const {
     data: { viewer },
-  } = useQuery(ViewerQuery)
+  } = useQuery(ViewerQuery);
 
   if (viewer) {
     return (
       <div>
-        You're signed in as {viewer.name} and you're {viewer.status} goto{' '}
+        You're signed in as blublu {viewer.name} and you're {viewer.status} goto{" "}
         <Link href="/about">
           <a>static</a>
-        </Link>{' '}
+        </Link>{" "}
         page.
       </div>
-    )
+    );
   }
 
-  return null
-}
+  return null;
+};
 
-export default withApollo(Index)
+export default withApollo(Index);
