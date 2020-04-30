b513c716cd48aaa9a9f44223d7d1e1b2fcbb8c32
Jimmy Thomas <jthomas@deezer.com>
2020-04-30 22:45:38 +0200

    First commit

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./components/**/*.tsx",
+      "./pages/**/*.tsx",
+      "./components/*.tsx",
+      "./pages/*.tsx",
+    ],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+
+module.exports = {
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
