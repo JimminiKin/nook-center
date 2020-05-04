6ab3098ccf218666e3d84bacf0b8a805ffc0703b
Jimmy Thomas <jthomas@deezer.com>
2020-05-04 12:05:49 +0200

    Fix some typos

--- src/pages/hunting.tsx
+++ src/pages/hunting.tsx
@@ -21,22 +21,18 @@ const Hunting: NextPage = (props) => {
 				</p>
 				<ul className="p-4 py-2 mx-8 -my-2 text-xl list-disc">
 					<li>Mystery island method : slow, no probability manipulation possible, need to get those miles tickets</li>
-					<li>Campsite method : still slow, but probability is can be much more manipulated, making it much easier</li>
+					<li>Campsite method : still slow, but probability can be much more manipulated, making it much easier</li>
 				</ul>
 				<p className="p-4 text-xl">
 					Obviously time traveling will help but percentages presented by this website should still hold true if you're
 					not a filthy cheater. In my example, using the campsite method, I was able to get Julian after 35 campsites,
-					which means at least 35 in game weeks. With time traveling, this means at least 100 time jumps, so more or
-					less 20 hours. Without TT, this means at least .... 35 real life weeks ! Your mileage may vary.
+					which means at least 35 weeks, in game time. With time traveling, this means at least 100 time jumps, so more
+					or less 20 hours. Without TT, this means at least .... 35 real life weeks ! Your mileage may vary.
 				</p>
 				<section>
 					<h2 className="text-green-900 font-bold text-4xl p-8 bg-green-500 text-center rounded-lg my-4">
 						Mystery Island Method
 					</h2>
-					<p className="p-4 text-xl">
-						Probability of a specific villager spawning on a mystery island seems to only be driven by the number of
-						villager with the same species that are not currently in your village.
-					</p>
 					<p className="p-4 text-xl">
 						Probability of a specific villager spawning on a mystery island seems to only be driven by the number of
 						villager with the same species that are not currently in your village.
@@ -48,12 +44,12 @@ const Hunting: NextPage = (props) => {
 					</ul>
 					<p className="p-4 text-xl">
 						This means you have the same probability to have AN octopus or a cat, BUT the probability to have a specific
-						octopus (only 3 possible) will be much higher than the one to get a specific cat (23 possible)
+						octopus (only 3 possible) will be much higher than the one to get a specific cat (23 possible).
 					</p>
 					<p className="p-4 text-xl">
 						To have villagers spawn on a mystery island, you have to have an empty spot on your island, or a spot for
-						sale. If you need to get rid of a current villager beforehand, you'll need to ignore them fo a while. For
-						more information on how to kick a villager, you can watch{' '}
+						sale. If you need to get rid of a current villager beforehand, you'll need to ignore them fo a while (at
+						least 15 days apparently). For more information on how to kick a villager, you can watch{' '}
 						<a href="https://www.youtube.com/watch?v=AasUEUGerZA" target="_blank" className="font-bold">
 							this video
 						</a>{' '}
@@ -79,13 +75,13 @@ const Hunting: NextPage = (props) => {
 					</p>
 					<p className="p-4 text-xl">Villager selection in a campsite should follow this process :</p>
 					<ul className="p-4 py-2 mx-8 -my-2 text-xl list-disc">
-						<li>First, game will pick the personality of the villager 1 out of 8.</li>
+						<li>First, game will pick the personality of the villager, 1 out of 8.</li>
 						<li>Second, game will pick a villager with the selected personality.</li>
 					</ul>
 					<p className="p-4 text-xl">Two facts make this method much easier than mystery island for hunting :</p>
 					<ul className="p-4 py-2 mx-8 -my-2 text-xl list-disc">
 						<li>
-							Any villager that spawns in the campsite will NOT respawn there twice before all the same personality
+							Any villager that spawns in the campsite will NOT respawn there again before all the same personality
 							villagers have spawned (might be all villagers, not only same personality, but not sure yet).
 						</li>
 						<li>
@@ -101,7 +97,7 @@ const Hunting: NextPage = (props) => {
 					</p>
 					<p className="p-4 text-xl">
 						If you're ready to make this sacrifice, you should be able to pin down your wanted villager in around 20 to
-						40 campsite. I for example, got Julian after 35 campsites. My soul is dead after so much time travel, BUT I
+						40 campsites. I for example, got Julian after 35 campsites. My soul is dead after so much time travel, BUT I
 						HAVE A UNICORN !!!!! Raymond being a smug, the probability to have him instead of Julian was the same (5% as
 						the time of Julian spawning).
 					</p>
@@ -109,10 +105,11 @@ const Hunting: NextPage = (props) => {
 						Unfortunately, now that the smug personality is represented in my village (by Julian), Raymond probability
 						is much lower, BUT, as I went through a lot of smug villagers while hunting, probability is still much
 						higher to get Raymond than a specific villager with another personality. In my example (which is the default
-						data loaded on the website), Raymond is at 0.52% while all peppy villagers is at 0.12%.
+						data loaded on the website), Raymond is at 0.52% while all peppy villagers are at 0.12%.
 					</p>
 					<p className="p-4 text-xl">Now that we talked about the probability, we can get into the method itself.</p>
-					<p className="p-4 text-xl">Now that we talked about the probability, we can get into the method itself.</p>
+
+					<p className="p-4 text-xl">// TODO More info =).</p>
 					<p className="p-4 text-xl">
 						For more information about the specifics of the method{' '}
 						<a href="https://www.youtube.com/watch?v=b_Veo5QayUI" target="_blank" className="font-bold">
